6747833ad07ae3e1ced7ef973f96e32bfddeed0d
dakdot <13dakdot@gmail.com>
2024-06-29 07:57:58 -0600

    split up file into two classes. Fixed dual window popup. ReOrg into class structure.

--- pokeVisionMain.py
+++ pokeVisionMain.py
@@ -2,3 +2,8 @@
 # I would like this file to be where the application launches from
 # and stuff. Trying to envision it currently. Really want to focus
 # on solid principles and building a nice looking application. 
+
+from pokeScan import ImageHandler
+import pokeScan.GUI as gui
+
+gui()
